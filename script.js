e860eb39d132175eabc98ce98f73c5d46b05d6db
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-09-10 14:18:58 +0600

    button function added

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('dhvg');
+const getElement = (id) => {
+    const element = document.getElementById(id);
+    return element;
+}
+
+
+const handleSUbmit = () => {
+    const inputText = getElement('todo-text').value;
+    // inputText.innerText = '';
+    // console.log("click");
+    // const input = document.getElementById('todo-text');
+    // console.log(inputText);
+    const ul =getElement('todo-list');
+     const li =document.createElement("li");
+     li.innerText = inputText;
+     ul.appendChild(li);
+
+
+}
