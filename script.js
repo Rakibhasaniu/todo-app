645472727f48ee13b5da49f06aeba5577e9589ec
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-09-10 16:14:19 +0600

    add and remove function add

--- script.js
+++ script.js
@@ -5,15 +5,61 @@ const getElement = (id) => {
 
 
 const handleSUbmit = () => {
+    const todos = JSON.parse(localStorage.getItem("TODOS"));
     const inputText = getElement('todo-text').value;
+    if(!todos){
+        const todoList = [
+            {
+                title : inputText,
+                completed : false,
+            },
+        ];
+        localStorage.setItem("TODOS",JSON.stringify (todoList));
+    } else{
+        const todoList = [
+            ...todos,
+            
+                {
+                    title : inputText,
+                    completed : false,
+                },
+            
+            
+        ];
+        localStorage.setItem("TODOS",JSON.stringify (todoList));
+    }
     // inputText.innerText = '';
     // console.log("click");
     // const input = document.getElementById('todo-text');
     // console.log(inputText);
-    const ul =getElement('todo-list');
-     const li =document.createElement("li");
-     li.innerText = inputText;
-     ul.appendChild(li);
+    // const ul =getElement('todo-list');
+    //  const li =document.createElement("li");
+    //  li.classList.add("py-2");
+    //  li.innerText = inputText;
+    //  ul.appendChild(li);
 
+    render();
+};
+const render = () => {
+    const todos = JSON.parse(localStorage.getItem("TODOS"));
+    // console.log(todos);
+    const ul = getElement("todo-list");
+    ul.innerHTML = '';
 
+    todos.forEach((item) => {
+        // console.log(item);
+        const li =document.createElement("li");
+        li.classList.add('py-4')
+        li.innerText = item.title;
+        ul.appendChild(li);
+    })
 }
+
+
+
+
+render();
+const handleDelet = () => {
+    localStorage.removeItem("TODOS");
+    render();
+};
